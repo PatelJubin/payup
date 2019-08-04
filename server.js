e84905c269acb0a0a4686fcfb852de0efdd25493
PatelJubin <jubin.patel@mail.utoronto.ca>
2019-08-04 02:09:04 -0400

    frontend stuff

--- server.js
+++ server.js
@@ -19,10 +19,7 @@ app.use(bodyParser.json());
 
 const db = require("./config/keys").mongoURI;
 mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true }
-  )
+  .connect(db, { useNewUrlParser: true })
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
@@ -35,7 +32,7 @@ require("./config/passport")(passport);
 app.use("/api/users", user);
 app.use("/api/group", group);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server running"));
 /* https.createServer(app).listen(PORT, function(err) {
   if (err) console.log(err);
